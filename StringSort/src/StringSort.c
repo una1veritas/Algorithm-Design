6975fe6e94a69ce59ced5faf964eb20085f08ab7
Sin Shimozono <sin@Basalt.local>
2015-12-17 12:31:19 +0900

    StringSort

---
+++ StringSort/src/StringSort.c
@@ -0,0 +1,44 @@
+/*
+ * 文字列の配列のソートプログラム：入出力部の参考
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef char* string;
+
+/* 全順序関係（関数），ソート関数　の型宣言，本体の定義など */
+
+int compare_str(void *a, void *b) {
+	/* a, b ともに配列の要素(string)へのポインタ */
+    return strcmp(*(string*) a, *(string*) b) ;
+}
+
+int main(int argcount, string argval[]) {
+
+	int number = argcount - 1;
+	string sarray[number+1];
+	for(int i = 0; i < number; i++) {
+		sarray[i] = argval[i+1];
+	}
+	sarray[number] = NULL;
+
+	printf("totally %d words.\n",number);
+	for(int i = 0; i < number; i++) {
+		printf("%d: %s\n", i, sarray[i]);
+	}
+	printf("\n");
+
+	/* stdlib の qsort ライブラリ関数を使ってソートする例 */
+
+	qsort(sarray, number, sizeof( string ), compare_str);
+
+	printf("sorted:\n");
+	for(int i = 0; i < number; i++) {
+		printf("%d: %s\n", i, sarray[i]);
+	}
+	printf("\n");
+
+	return EXIT_SUCCESS;
+}
