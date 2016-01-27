e3aa238112245978f56d11b815c4cfb742248c68
Sin Shimozono <una.veritas@me.com>
2016-01-28 01:20:05 +0900

    StringHash

---
+++ C++StringHash/test.cpp
@@ -0,0 +1,26 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 2016/01/27
+ *      Author: sin
+ */
+
+#include <iostream>
+
+#include <string>
+#include <unordered_map>
+
+int main(int argc, char * argv[]) {
+
+	std::hash<std::string> hasher;
+	std::string str;
+
+	std::cout << "--- Hash values ---" << std::endl;
+	while(*argv != NULL) {
+		str = *argv;
+		unsigned long hashValue = hasher(str);
+		std::cout << hashValue << "  \"" << str << "\" " << std::endl;
+		argv++;
+	}
+	return EXIT_SUCCESS;
+}
