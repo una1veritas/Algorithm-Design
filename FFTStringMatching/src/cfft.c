938d78860b0273bb5c99579cce9c593810ac239a
Sin Shimozono <una.veritas@icloud.com>
2016-11-14 17:30:14 +0900

    FFTStringMatching
    float complex, occurrence pos bug fix

--- FFTStringMatching/src/cfft.c
+++ FFTStringMatching/src/cfft.c
@@ -15,12 +15,12 @@
 /*
  *  Explicit divide-and-conqure FFT by recursion
  */
-void rfft(dcomplex *v, int n, dcomplex *tmp) {
+void rfft(float complex *v, int n, float complex *tmp) {
 	if ( n <= 1 ) /* do nothing and return */
 		return;
 	// n > 1
 	const int m = n >> 1;
-	dcomplex *v1, *v0;
+	float complex *v1, *v0;
 	v0 = tmp;
 	v1 = tmp + m;
 	for (int i = 0; i < n / 2; i++) {
@@ -31,8 +31,8 @@ void rfft(dcomplex *v, int n, dcomplex *tmp) {
 	rfft(v1, m, v); /* FFT on odd-indexed elements of v[] */
 	for (int i = 0; i < m; i++) {
 		// w = (cos(2 * PI * i / (double) n)) + (-sin(2 * PI * i / (double) n))*I;
-		dcomplex w = cexp(- I * /* 2 * */ PI * i / (double) m /* n */);
-		dcomplex z = w * v1[i];
+		float complex w = cexpf(- I * /* 2 * */ PI * i / (double) m /* n */);
+		float complex z = w * v1[i];
 		v[i] = v0[i] + z;
 		v[i + m] = v0[i] - z;
 	}
@@ -43,8 +43,8 @@ void rfft(dcomplex *v, int n, dcomplex *tmp) {
 /*
  * FFT by butterfly changes in advance forcing divide-and-conqure
  */
-void cfft(dcomplex *vec, int n, dcomplex *scratch) {
-	dcomplex *src, *dst, *tmp;
+void cfft(float complex *vec, int n, float complex *scratch) {
+	float complex *src, *dst, *tmp;
 	src = vec;
 	dst = scratch;
 	// n > 1
@@ -68,8 +68,8 @@ void cfft(dcomplex *vec, int n, dcomplex *scratch) {
 		for (int h = 0; h < n; h += (m<<1) ) {
 			for(int i = 0; i < m; i++) {
 				// fprintf(stdout, "(%d : %d, %d) ", h, h+m, i);
-				dcomplex w = cexp(- I * /* 2 * */ PI * i / ((double) m) /* n */);
-				dcomplex z = w * src[h + m + i];
+				float complex w = cexpf(- I * /* 2 * */ PI * i / ((double) m) /* n */);
+				float complex z = w * src[h + m + i];
 				dst[h + i] = src[h + i] + z;
 				dst[h + m + i] = src[h + i] - z;
 			}
@@ -85,16 +85,16 @@ void cfft(dcomplex *vec, int n, dcomplex *scratch) {
 
 
 
-void ifft(dcomplex *v, int n, dcomplex *tmp) {
+void ifft(float complex *v, int n, float complex *tmp) {
 
 	for (int i = 0; i < n; i++) {
-		v[i] = conj(v[i]);
+		v[i] = conjf(v[i]);
 	}
 
 	cfft(v, n, tmp);
 
 	for (int i = 0; i < n; i++) {
-		dcomplex t = conj(v[i]) / ((double) n);
+		float complex t = conjf(v[i]) / ((double) n);
 		v[i] = t;
 	}
 	return;
--- FFTStringMatching/src/main.c
+++ FFTStringMatching/src/main.c
@@ -19,20 +19,21 @@
 
 #include "cfft.h"
 
-struct dcompvec {
-	dcomplex * elem;
-	int dimsize;
+struct compvect {
+	float complex * elem;
+	int dim;
 };
-typedef struct dcompvec dcompvec;
+typedef struct compvect compvect;
 const int CONJ_REVERSE = 1;
 
 struct text {
 	char * str;
-	int textsize;
+	int maxsize;
+	int length;
 };
 typedef struct text text;
 
-void dcompvec_mulinto(dcompvec * x, const dcompvec * y);
+void compvect_mulinto(compvect * x, const compvect * y);
 long smallestpow2(const long n) {
 	long t = 1;
 	while ( t < n ) {
@@ -45,47 +46,54 @@ long smallestpow2(const long n) {
 #define max(x,y)  ( ((x) < (y) ? (y) : (x)) )
 #define abs(x)  ( (x) < 0 ? (-(x)) : (x) )
 
-int make_signal(text * text1, int dimsize, dcompvec * vec, const int flag);
+int make_signal(const text * text1, const int dimsize, compvect * vec, const int flag);
 int get_values(int argc, const char * argv[], text * text1, text * text2);
-void print_vector(const char *title, dcomplex *x, int n);
-
+void print_vector(const char *title, compvect *x);
+void findOccurrences(compvect * vec, int pattlen, int * results, int upto);
 
 int main(int argc, const char * argv[]) {
 	text text1, text2;
-	dcompvec vec1, vec2;
+	compvect vec1, vec2;
 
+	text1.maxsize = 512;
+	text1.str = (char *) malloc(sizeof(char)*text1.maxsize);
+	text2.maxsize = 512;
+	text2.str = (char *) malloc(sizeof(char)*text2.maxsize);
 	/* Get N and fill v[] with program inputs. */
 	if ( !get_values(argc, argv, &text1, &text2) )
 		exit(EXIT_FAILURE);
 
-	printf("inputs: \"%s\", \"%s\" \n", text1.str, text2.str);
-	int pow2len = smallestpow2(max(text1.textsize, text2.textsize));
-	int pattlen = min(text1.textsize, text2.textsize);
+	printf("inputs: \"%s\" (%d), \"%s\" (%d) \n", text1.str, text1.length, text2.str, text2.length);
+	int pow2len = smallestpow2(max(text1.length, text2.length));
+	int pattlen = min(text1.length, text2.length);
+	printf("pow2len = %d, pattlen = %d\n", pow2len, pattlen);
 
 	make_signal(&text1, pow2len, &vec1, !CONJ_REVERSE);
+	print_vector("text1 ", &vec1);
 	make_signal(&text2, pow2len, &vec2, CONJ_REVERSE);
+	print_vector("text2 ", &vec2);
 	/* FFT, iFFT of v[]: */
-	print_vector("text1 ", vec1.elem, vec1.dimsize);
-	print_vector("text2 ", vec2.elem, vec2.dimsize);
 
-	dcomplex * scratch = (dcomplex *) malloc(sizeof(dcomplex) * vec1.dimsize);
+	float complex * scratch = (float complex *) malloc(sizeof(float complex) * vec1.dim);
 
-	cfft(vec1.elem, vec1.dimsize, scratch);
+	cfft(vec1.elem, vec1.dim, scratch);
 //	print_vector("fft1 ", text1.elem, text1.dimsize);
-	cfft(vec2.elem, vec2.dimsize, scratch);
+	cfft(vec2.elem, vec2.dim, scratch);
 //	print_vector("fft2 ", text2.elem, text2.dimsize);
 
-	dcompvec_mulinto(&vec1, &vec2);
+	compvect_mulinto(&vec1, &vec2);
 //	print_vector("prod ", text1.elem, text1.dimsize);
 
-	ifft(vec1.elem, vec1.dimsize, scratch);
+	ifft(vec1.elem, vec1.dim, scratch);
 
-	print_vector("iFFT ", vec1.elem, vec1.dimsize);
+	print_vector("iFFT ", &vec1);
 
+	int results[8];
+	findOccurrences(&vec1, pattlen, results, 8);
 	printf("Occurring positions: ");
-	for(int i = 0; i < vec1.dimsize; i++) {
-		if ( abs(creal(vec1.elem[(vec1.dimsize - pattlen + i) % vec1.dimsize]) - (double) pattlen) < 0.0001220703125 )
-			printf("%d, ", i - pattlen + 1);
+	for(int i = 0; i < 8; i++) {
+		if ( results[i] < vec1.dim)
+			printf("%d, ", results[i] - pattlen + 1);
 	}
 	printf(".\n");
 
@@ -99,77 +107,93 @@ int main(int argc, const char * argv[]) {
 }
 
 int get_values(int argc, const char * argv[], text * text1, text * text2) {
+	int len1, len2;
+
 	if ( argc <= 2 ) {
 		fprintf(stdout, "No or an invalid number of inputs specified; quit.\n");
 		return 0;
 	} else if( argc == 3 ) {
 		fprintf(stdout, "Take pattern and text from the command line arguments.\n");
-		if ( strlen(argv[1]) > strlen(argv[2]) ) {
-			text1->textsize = strlen(argv[1]);
-			text1->str = (char *) malloc(sizeof(char) * text1->textsize);
-			strcpy(text1->str, argv[1]);
-			text2->textsize = strlen(argv[2]);
-			text2->str = (char *) malloc(sizeof(char) * text2->textsize);
-			strcpy(text2->str, argv[2]);
+		len1 = strlen(argv[1]);
+		len2 = strlen(argv[2]);
+		if ( len1 >= len2 ) {
+			strncpy(text1->str, argv[1], min(text1->maxsize, len1+1));
+			text1->length = len1;
+			strncpy(text2->str, argv[2], min(text2->maxsize, len2+1));
+			text2->length = len2;
 		} else {
-			text1->textsize = strlen(argv[2]);
-			text1->str = (char *) malloc(sizeof(char) * text1->textsize);
-			strcpy(text1->str, argv[2]);
-			text2->textsize = strlen(argv[1]);
-			text2->str = (char *) malloc(sizeof(char) * text2->textsize);
-			strcpy(text2->str, argv[1]);
+			strncpy(text1->str, argv[2], min(text1->maxsize, len2+1));
+			text1->length = len2;
+			strncpy(text2->str, argv[1], min(text2->maxsize, len1+1));
+			text2->length = len1;
 		}
 	}
 	return 1;
 }
 
-int make_signal(text * str, const int dimsize, dcompvec * vec, const int flag) {
-	int len;
+int make_signal(const text * str, const int dimsize, compvect * vec, const int flag) {
 	int dst;
-	complex factor;
+	float complex factor;
 
 	// the first as normal
-	vec->dimsize = dimsize;
-	vec->elem = (dcomplex*) malloc(sizeof(dcomplex)*dimsize);
-	len = str->textsize;
-	for(int i = 0; i < vec->dimsize; ++i) {
+	vec->dim = dimsize;
+	vec->elem = (float complex*) malloc(sizeof(float complex)*dimsize);
+	for(int i = 0; i < vec->dim; ++i) {
 		if ( !flag ) {
 			dst = i;
 			factor = 2*PI*I;
 		} else {
-			dst = vec->dimsize - i - 1;
+			dst = vec->dim - i - 1;
 			factor = -2*PI*I;
 		}
-		if ( i < len )
-			vec->elem[dst] = cexp( factor * (float)(str->str[i]) /256.0f );  // by rotated unit vector
+		if ( i < str->length ) {
+			vec->elem[dst] = cexpf( factor * (float)(str->str[i]) /256.0f );  // by rotated unit vector
 			// (*array)[i] = (float)(str[i]) / 128.0f  ;  // by char value
-		else
+		} else {
 			vec->elem[dst] = 0;
+		}
 	}
+	printf("\n");
 	return 1;
 }
 
 /* Print a vector of complexes as ordered pairs. */
-void print_vector(const char *title, dcomplex *x, int n) {
+void print_vector(const char *title, compvect *x) {
 	int i;
-	printf("%s (dim=%d):\n", title, n);
-	for (i = 0; i < n; i++)
+	printf("%s (dim=%d):\n", title, x->dim);
+	for (i = 0; i < x->dim; i++)
 		printf("%5d    ", i );
 	putchar('\n');
-	for (i = 0; i < n; i++)
-		printf(" %7.3f,", creal(x[i]) );
+	for (i = 0; i <  x->dim; i++)
+		printf(" %7.3f,", crealf(x->elem[i]) );
 	putchar('\n');
-	for (i = 0; i < n; i++)
-		printf(" %7.3f,", cimag(x[i]) );
+	for (i = 0; i <  x->dim; i++)
+		printf(" %7.3f,", cimagf(x->elem[i]) );
 	putchar('\n');
-	for (i = 0; i < n; i++)
-		printf(" %7.3f,", cabs(x[i]) );
+	for (i = 0; i <  x->dim; i++)
+		printf(" %7.3f,", cabsf(x->elem[i]) );
 	printf("\n\n");
 	return;
 }
 
-void dcompvec_mulinto(dcompvec * x, const dcompvec * y) {
-	for(int i = 0; i < x->dimsize; i++) {
+void compvect_mulinto(compvect * x, const compvect * y) {
+	for(int i = 0; i < x->dim; i++) {
 		x->elem[i] = x->elem[i] * y->elem[i];
 	}
+}
+
+void findOccurrences(compvect * vec, int pattlen, int * results, int upto) {
+	int reslast = 0;
+	for(int i = 0; i < vec->dim; ++i) {
+		if ( abs(crealf(vec->elem[i]) - (double) pattlen) < 0.0001220703125 ) {
+			printf("%d, ", i);
+			if ( reslast < upto ) {
+				results[reslast] = (i + pattlen) % vec->dim;
+				++reslast;
+			}
+		}
+	}
+	printf("\n");
+	for( ; reslast < upto; ++reslast)
+		results[reslast] = vec->dim;
 }
