26634ec66252b7e1b67b35d2ddac32494c2a5e4a
Sin Shimozono <una.veritas@icloud.com>
2016-11-29 17:43:45 +0900

    String LDist

--- String_LDist_diagonal/src/editdistance.c
+++ String_LDist_diagonal/src/editdistance.c
@@ -14,10 +14,10 @@
 
 #define min(x, y)  ((x) <= (y)? (x) : (y))
 
-// assumes the table size (n+1) x (m+1)
+// assuming the table array size (n+1) x (m+1)
 long dptable(long * table, const long colsize, const long rowsize, const char t[], const long n, const char p[], const long m) {
 	long dix, dcol, drow; // diagonal column index, row index
-	long col, row; // inner diagonal index
+	long col; // inner diagonal index
 	long result = n+m+1; // an impossible large value
 	long ins, del, repl;
 
@@ -25,79 +25,110 @@ long dptable(long * table, const long colsize, const long rowsize, const char t[
 	// clear table elements
 	for(long r = 0; r < rowsize; ++r)
 		for(long c = 0; c < colsize; ++c)
-			table[rowsize*c + r] = 0;
+			table[rowsize*c + r] = -1;
 #endif
 
 	// initialize
-	for(col = 0; col <= n; ++col) {
-		if ( col <= m ) {
-			dix = col*(col+1)/2;
+	// do in parallel for each dcol
+	for(dcol = 0; dcol <= n; ++dcol) {
+		if ( dcol <= m ) {
+			dix = dcol*(dcol+1)/2;
 		} else {
-			dix = m*(m+1)/2+(m+1)*(col-m);
+			dix = m*(m+1)/2+(m+1)*(dcol-m);
 		}
-		table[dix] = col;
+		table[dix] = dcol;
+	}
+	// do in parallel for each drow
+	for(drow = 1; drow <= m; ++drow) {
+		// m <= n
+		dix = (drow+1)*(drow+2)/2 - 1;
+		table[dix] = drow;
+	}
+
+	// upper-left triangle
+	for(dcol = 2; dcol <= m + 1; ++dcol) {
+		dix = dcol*(dcol+1)/2;
+		for(drow = 1; drow < dcol; ++drow) {
+			col = dcol - drow;
+			fprintf(stdout, "%3ld:%3ld [%2ld, %2ld] ", dcol, dix+drow, col, drow);
+			ins = table[ dix + drow - dcol - 1 ] + 1;
+			del = table[ dix + drow - dcol ] + 1;
+			repl = table[ dix + drow - 2*dcol]
+						  + (t[col-1] == p[drow-1] ? 0 : 1);
+			fprintf(stdout, " %c=%c? ",t[col-1],p[drow-1] );
+			ins = ((ins <= del) ? ins : del);
+			table[dix+drow]  = (ins < repl ? ins : repl);
+		}
+		fprintf(stdout, "\n");
 	}
 	fprintf(stdout, "\n");
 
-	for(row = 1; row <= m; ++row) {
-		// m <= n
-		dix = (row+1)*(row+2)/2 - 1;
-		table[dix] = row;
-	}
-
-	for(dcol = 1; dcol <= m; ++dcol) {
-		for(drow = 1; drow <= dcol; ++drow) {
-			dix = dcol*(dcol+1)/2 + drow;
-			row = drow;
-			col = dcol - row;
-			//fprintf(stdout, "%3lu:%3lu [%2lu, %2lu] ", dcol+2, (dcol+2)*rowsize+drow+1, col, row);
-			ins = table[ dix - dcol - 1 ] + 1;
-			del = table[ dix - dcol ] + 1;
-			repl = table[ dix - dcol - (dcol - 1) - 1]
-						  + (t[dcol-row] == p[drow] ? 0 : 1);
-			//fprintf(stdout, " %c=%c? ",t[dcol-drow],p[drow] );
+	// skewed rectangle
+	for(dcol = m+2; dcol < n + 1;++dcol) {
+		dix = (m+1)*(m+2)/2 + (m+1) + (m+1)*(dcol - (m+2));
+		for(drow = 1; drow < m + 1; ++drow) {
+			col = dcol - drow;
+			fprintf(stdout, "%3ld:%3ld [%2ld, %2ld] ", dcol, dix+drow, col, drow);
+			fprintf(stdout, " %c=%c? ",t[col-1],p[drow-1] );
+			ins = table[ dix + drow - (m+1) - 1] + 1;
+			del = table[ dix + drow - (m+1) ] + 1;
+			repl = table[ dix + drow - 2 *(m+1) - 1 ]
+						  + (t[col-1] == p[drow-1] ? 0 : 1);
 			ins = ((ins <= del) ? ins : del);
-			table[ dix]  = (ins < repl ? ins : repl);
-		}
-	}
-
-/*
-	for(dcol = n; dcol < n+m; ++dcol) {
-		for(drow = dcol + 1 - n; drow < m; ++drow) {
-			row = drow;
-			col = dcol - row;
-			//DEBUGOUT fprintf(stdout, "%3lu:%3lu [%2lu, %2lu] ", (dcol+2) %(n+2), ((dcol+2)%(n+2))*rowsize+drow+1, col, row);
-			ins = table[ (((dcol + 2) - 1) % (n+2)) *rowsize + (drow + 1) - 1 ] + 1;
-			del = table[ (((dcol + 2) - 1)% (n+2))*rowsize + (drow + 1) ] + 1;
-			repl = table[ (((dcol + 2) - 2)% (n+2))*rowsize + (drow + 1) - 1 ]
-						  + (t[dcol-row] == p[drow] ? 0 : 1);
-			//fprintf(stdout, " %c=%c? ",t[dcol-drow],p[drow] );
-			ins = ((ins <= del) ? ins : del);
-			table[ ((dcol + 2)% (n+2))*rowsize + (drow + 1)]  = (ins < repl ? ins : repl);
+			table[ dix + drow ]  = (ins < repl ? ins : repl);
 #ifdef DEBUG_DPTABLE
 			//table[(dcol + 2)*rowsize + (drow + 1)] = table[ ((dcol + 2)% (n+2))*rowsize + (drow + 1)];
 #endif
 			}
+		fprintf(stdout, "\n");
 	}
-*/
+	fprintf(stdout, "\n");
+
+
+	// bottom-right triangle
+	dix = (m+1)*(m+2)/2 + (m+1)*(n-m-1) + m;
+	for(dcol = n+1; dcol < n + m + 2;++dcol) {
+		for(drow = dcol - n; drow < m+1; ++drow) {
+			col = dcol - drow;
+			fprintf(stdout, "%3ld:%3ld [%2ld, %2ld] ", dcol, dix+drow, col, drow);
+			/*
+			ins = table[ (((dcol + 2) - 1) % (n+2)) *rowsize + (drow + 1) - 1 ] + 1;
+			del = table[ (((dcol + 2) - 1)% (n+2))*rowsize + (drow + 1) ] + 1;
+			repl = table[ (((dcol + 2) - 2)% (n+2))*rowsize + (drow + 1) - 1 ]
+						  + (t[col-row] == p[drow] ? 0 : 1);
+						  */
+			fprintf(stdout, " %c=%c? ",t[col-1],p[drow-1] );
+			/*
+			ins = ((ins <= del) ? ins : del);
+			table[ ((dcol + 2)% (n+2))*rowsize + (drow + 1)]  = (ins < repl ? ins : repl);
+			*/
+#ifdef DEBUG_DPTABLE
+			//table[(dcol + 2)*rowsize + (drow + 1)] = table[ ((dcol + 2)% (n+2))*rowsize + (drow + 1)];
+#endif
+			}
+		dix += m + 1 - (dcol - n + 1);
+		fprintf(stdout, "\n");
+	}
+	fprintf(stdout, "\n");
 
 #ifdef DEBUG_DPTABLE
 	// show DP table
-	for(row = 0; row <= m; row++) {
-		for (col = 0; col <= n; col++) {
-			if ( col + row <= m ) {
-				dix = (col + row)*(col + row + 1)/2 + row;
-			} else if (col+row<=n) {
-				dix = (m+1)*(m+2)/2 + (m+1)*(col - m - 1 + row) + row;
+	long r, c;
+	for(r = 0; r <= m; r++) {
+		for (c = 0; c <= n; c++) {
+			if ( c + r <= m ) {
+				dix = (c + r)*(c + r + 1)/2 + r;
+			} else if (c + r <= n) {
+				dix = (m+1)*(m+2)/2 + (m+1)*(c - m - 1 + r) + r;
 			} else {
-				dix = (m+1)*(m+2)/2 + (m+1)*(col - m - 1 + row) + row - (col+row-n)*(col+row-n+1)/2;
+				dix = (m+1)*(m+2)/2 + (m+1)*(c - m - 1 + r) + r - (c + r - n)*(c + r - n + 1)/2;
 			}
-			fprintf(stdout, "%2lu\t", table[dix]);
+			fprintf(stdout, "%2ld:%2ld\t", dix,table[dix]);
 		}
 		fprintf(stdout, "\n");
 	}
 
-	fprintf(stdout, "[%3lu], ", (m-2)*rowsize + m );
+	fprintf(stdout, "[%3ld], ", (m-2)*rowsize + m );
 #endif
 	result = table[(m-2)*rowsize + m ];
 	return result;
