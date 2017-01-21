300b379a1cc37a5e036e54f0ee55b9b5594622e2
Sin Shimozono <una.veritas@me.com>
2017-01-22 02:07:22 +0900

    levdist.h

--- String_LevDist/src/levdist.h
+++ String_LevDist/src/levdist.h
@@ -19,7 +19,45 @@
 #endif //ABS
 
 //long ceilpow2(const long val);
+int clz64(unsigned int x)
+{
+	int n;
+	union {
+		unsigned long long as_uint64;
+		double             as_double;
+	} data;
+	data.as_double = (double)x + 0.5;
+	n = 1054 - (data.as_uint64 >> 52);
+	return n;
+}
 
+int clz32(unsigned int x)
+{
+	int n;
+	union {
+		unsigned int as_uint32;
+		float        as_float;
+	} data;
+
+	data.as_float = (float)x + 0.5;
+	n = 158 - (data.as_uint32 >> 23);
+	return n;
+}
+
+/*
+int
+nlz(int x)
+{
+int c = 0;
+if (x == 0) return 32;
+if (x & 0xffff0000) { x &= 0xffff0000; c |= 0x10; }
+if (x & 0xff00ff00) { x &= 0xff00ff00; c |= 0x08; }
+if (x & 0xf0f0f0f0) { x &= 0xf0f0f0f0; c |= 0x04; }
+if (x & 0xcccccccc) { x &= 0xcccccccc; c |= 0x02; }
+if (x & 0xaaaaaaaa) { c |= 0x01; }
+return c ^ 31;
+}
+*/
 long r_edist(char s[], int m, char t[], int n);
 
 void setframe(long * frame, const long n, const long m);
