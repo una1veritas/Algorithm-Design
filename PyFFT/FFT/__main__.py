fa6c371530709b2382464686f430c645e4e9e16a
Sin Shimozono <una.veritas@icloud.com>
2019-05-13 14:49:13 +0900

    PyFFT

--- PyFFT/FFT/__main__.py
+++ PyFFT/FFT/__main__.py
@@ -9,7 +9,17 @@ def fft(varray):
         s = 0
         for x in range(0, n):
             s += varray[x] * cmath.exp(-1j * 2*math.pi/n * t * x)
-        f.append(s/math.sqrt(n))
+        f.append(s)
+    return f
+
+def invfft(varray):
+    n = len(varray)
+    f = list()
+    for t in range(0, n):
+        s = 0
+        for x in range(0, n):
+            s += varray[x] * cmath.exp(-1j * 2*math.pi/n * t * x)
+        f.append(s/n)
     return f
 
 if len(sys.argv) != 2 :
@@ -25,5 +35,8 @@ for i in range(len(data), n):
     data.append(0)
 
 print()
-for val in fft(data):
+tdata = fft(data)
+for val in tdata:
     print(round(abs(val),4))
+
+print(invfft(tdata))
