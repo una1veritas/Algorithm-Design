657adf65b3ba095a8f232bbbcde46d315b788405
Sin Shimozono <una.veritas@icloud.com>
2019-06-03 16:50:41 +0900

    Bitmanip

--- BitManip/BitManip.c
+++ BitManip/BitManip.c
@@ -29,7 +29,9 @@ int main(int argc, char **argv) {
 	for(int i = 1; i < argc; ++i) {
 		printf("%s ", argv[i]);
 		val = strtol(argv[i], NULL, 10);
-		if ( val < (((long)1)<<17) ) {
+		if ( val < (((long)1)<<8) ) {
+			bitprintf(val, 8); printf("\n");
+		} else if ( val < (((long)1)<<17) ) {
 			bitprintf(val, 16); printf("\n");
 		} else {
 			bitprintf(val, 32); printf("\n");
@@ -37,3 +39,12 @@ int main(int argc, char **argv) {
 	}
 	return EXIT_SUCCESS;
 }
+
+/*
+make
+
+./BitManip 3487 33 987324
+3487 00001101 10011111
+33 00100001
+987324 00000000 00001111 00010000 10111100
+ */
