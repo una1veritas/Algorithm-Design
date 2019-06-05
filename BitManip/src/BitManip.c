23488e0ef56e66408957518f9bacc3bc49d18ca1
Sin Shimozono <una.veritas@icloud.com>
2019-06-05 15:45:31 +0900

    bitmanip

--- BitManip/src/BitManip.c
+++ BitManip/src/BitManip.c
@@ -2,26 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-int bitprintf(long v, int bits) {
-	int t = 8;
-	int count = 0;
-	while (t < bits)
-		t <<= 1;
-	bits = t;
+int bitprintf(unsigned int bwidth, unsigned long val) {
+	bwidth = (bwidth > 64 ? 64 : bwidth);
 	do {
-		bits -= 8;
-		t = (v>>(bits)) & 0xff;
-		count += printf("%c%c%c%c%c%c%c%c ",
-				(t & 0x80 ? '1' : '0'),
-				(t & 0x40 ? '1' : '0'),
-				(t & 0x20 ? '1' : '0'),
-				(t & 0x10 ? '1' : '0'),
-				(t & 0x08 ? '1' : '0'),
-				(t & 0x04 ? '1' : '0'),
-				(t & 0x02 ? '1' : '0'),
-				(t & 0x01 ? '1' : '0') );
-	} while ( bits > 0);
-	return count;
+		--bwidth;
+		printf("%c", (val & (1<<bwidth) ? '1' : '0'));
+	} while (bwidth > 0);
+	return bwidth;
 }
 
 int main(int argc, char **argv) {
