0f7d275ae920fa8924a41a3b614935ce0b84b3a7
una1veritas <una.veritas@me.com>
2020-04-05 20:00:00 +0900

    numplay

--- NumberPlace_solver/src/NumberPlace_solver.cpp
+++ NumberPlace_solver/src/NumberPlace_solver.cpp
@@ -11,52 +11,78 @@
 
 using namespace std;
 
-void show_cells(const int rows, char cells[]);
+void show_cells(const int rows, const int * cells);
+int read_input(const int argc, const char * argv[], int * & cells);
 
+const static int rows = 9;
 int main(const int argc, const char * argv[]) {
-	if ( argc < 3 ) {
-		cerr << "give me an input in rows, '003 050 09... 090' format." << endl;
+
+	int * cells = NULL;
+	if ( read_input(argc, argv, cells) )
 		return EXIT_FAILURE;
-	}
-	// count cells
-	int rows = atoi(argv[1]);
-	char cell[rows][rows];
-	int cellnum = 0;
-	const char * ptr;
-	for(ptr = argv[2]; *ptr != '\0'; ++ptr) {
-		if ( isdigit(*ptr) ) {
-			cell[cellnum / rows][cellnum % rows] = *ptr - '0';
-			cellnum += 1;
+	show_cells(rows, cells);
+
+	int ans[rows*rows];
+	for(int r = 0; r < rows; ++r) {
+		for(int c = 0; c < rows; ++c) {
+				ans[r*rows+c] = 1 + c;
+		}
+		for(int c = 0; c < rows; ++c) {
+			if ( cells[r*rows+c] != 0 )
+				ans[r*rows+cells[r*rows+c]-1] = 0; //rows + 1;
+		}
+		sort(ans+(r*rows), ans+(r*rows+rows));
+		for(int c = 0; c < rows; ++c) {
+			if ( ans[r*rows+c] == 0 )
+				ans[r*rows+c] = rows+1;
 		}
 	}
-	if ( cellnum != rows*rows ) {
-		cerr << cellnum << "digits!! give a sequence of exactly rows x rows digits!!" << endl;
-		return EXIT_FAILURE;
-	}
 
-	show_cells(rows, cell);
+	while (next_permutation(ans, ans+rows)) {
+		show_cells(rows, (const int *) ans);
+		cout << endl;
+	}
+	delete [] cells;
 	return 0;
 }
 
-void show_cells(const int rows, char *cell) {
+int read_input(const int argc, const char * argv[], int * & cells) {
+	if ( argc <= 81 ) {
+		cerr << "give me an input in '0 0 3 0 5 0 0 9 ... 0 9 0' format." << endl;
+		return EXIT_FAILURE;
+	}
+	cells = new int[rows*rows];
+	int cellnum = 0;
+	for(int i = 1; i < argc && cellnum <= 81; ++i) {
+		cells[cellnum] = atoi(argv[i]);
+		cellnum += 1;
+	}
+	if ( cellnum != rows*rows ) {
+		cerr << cellnum << "digits!! give a sequence of exact rows x rows numbers!!" << endl;
+		return EXIT_FAILURE;
+	}
+	return 0;
+}
+
+void show_cells(const int rows, const int * cells) {
 	for(int r = 0; r < rows; ++r) {
-		cout << "-";
+		cout << "+";
 		for(int c = 0; c < rows; ++c) {
-			cout << "----";
+			cout << "---+";
 		}
 		cout << endl;
 		cout << "|";
 		for(int c = 0; c < rows; ++c) {
-			if ( cell[r][c] == 0 )
+			if ( cells[r*rows + c] == 0 )
 				cout << " " << ' ' << " |";
 			else
-				cout << " " << (char) ('0'+cell[r][c]) << " |";
+				cout << " " << (char) ('0'+cells[r*rows + c]) << " |";
 		}
 		cout << endl;
 		if (r+1 == rows) {
-			cout << "-";
+			cout << "+";
 			for(int c = 0; c < rows; ++c) {
-				cout << "----";
+				cout << "---+";
 			}
 			cout << endl;
 		}
