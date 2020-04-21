ea1c8fa5df603b262449344bc24eb253792952d2
una1veritas <una.veritas@me.com>
2020-04-21 14:54:33 +0900

    sieve

---
+++ SieveOfEratosthenes/src/SieveOfEratosthenes.c
@@ -0,0 +1,33 @@
+/*
+ ============================================================================
+ Name        : SieveOfEratosthenes.c
+ Author      : Sin Shimozono
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int sieveOfEratosthenes(int n) {
+	char sieve[n+1];
+	int i;
+	for(i = 2; i <= n; ++i) sieve[i] = 1;
+	for(i = 2; i <= n/2; ++i ) {
+		if (! sieve[i] ) continue;
+		for(int j = i*2; j <= n; j += i)
+			sieve[j] = 0;
+	}
+	// sieve[2] == 1
+	for(i = n; i > 2 && !sieve[i]; --i);
+	return i;
+}
+
+int main(const int argc, const char * argv[]) {
+	if (argc < 2) { puts("error: give a limit number.\n"); return EXIT_FAILURE; }
+	int n = atoi(argv[1]);
+	printf("the largest prime no more than %d is: %d.\n", n, sieveOfEratosthenes(n));
+	return EXIT_SUCCESS;
+}
