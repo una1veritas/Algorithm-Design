b00de720ced586869b857d1713242ef6836970ad
Sin Shimozono <una.veritas@me.com>
2020-06-04 00:53:02 +0900

    heap

--- SortAlgorithms/src/heapchecker.c
+++ SortAlgorithms/src/heapchecker.c
@@ -1,5 +1,5 @@
 /*
- * libcquick.c
+ * heapchecker.c
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,20 +7,23 @@
 
 int unlhd_str5cmp(const char * stra, const char * strb) {
 	int res = strncmp(stra, strb, 5);
-	return (res == 0) ? 0 : (res < 0 ? 1 : 0);
+	return (res == 0) ? 1 : (res < 0 ? 1 : 0);
 }
 
 int down_to_leaf(const char * data[], int a[], int i, int n) {
-	int j, right, t;
-	for ( ; ; i = j) {
-		j = (i<<1) + 1;
-		if ( !(j < n) )
+	int j, t;
+	for ( ;(j = (i<<1) + 1) < n ; i = j) {
+		//printf("i = %d, ", i);
+		if ( (j + 1 < n) ) {
+			//printf("r a[%d] <= a[%d]", j, j+1);
+			if (unlhd_str5cmp(data[a[j]], data[a[j+1]]) ) {
+				j += 1;
+			}
+		}
+		if ( unlhd_str5cmp(data[a[j]], data[a[i]]) )
 			break;
-		right = (i<<1) + 2;
-		if ( (right < n) && (unlhd_str5cmp(data[a[j]], data[a[right]])) )
-			j = right;
 		t = a[i], a[i] = a[j], a[j] = t;
-		printf("%d <--> %d, ", i, j);
+		//printf("%d <-- %d, ", j, i);
 	}
 	return 1;
 }
@@ -28,9 +31,20 @@ int down_to_leaf(const char * data[], int a[], int i, int n) {
 void make_heap(const char * data[], int a[], int n) {
 	int i;
 	for (i = (n>>1); i > 0; --i) {
-		printf("%d: ",i-1);
+		//printf("%d: ",i-1);
 		down_to_leaf(data, a, i-1, n);
-		printf("\n");
+		//printf("\n");
+	}
+	return;
+}
+
+void heap_sort(const char * data[], int a[], int n) {
+	int i, t;
+	make_heap(data, a, n);
+	for (i = n - 1; i > 0; --i) {
+		// a[0] is always the maximum.
+		t = a[0], a[0] = a[i], a[i] = t;
+		down_to_leaf(data, a, 0, i);
 	}
 	return;
 }
@@ -47,7 +61,7 @@ int heapcheck(const int a[], const int n) {
 	return 1;
 }
 
-int main(const int argc, char *argv[]) {
+int main(const int argc, const char *argv[]) {
 	int n = argc - 1;
 	int idx[n];
 	for (int i = 0; i < n; ++i) {
@@ -58,7 +72,7 @@ int main(const int argc, char *argv[]) {
 	}
 	printf("\n");
 
-	make_heap((const char **)argv+1, idx, n);
+	heap_sort(argv+1, idx, n);
 
 	printf("\n");
 	for (int i = 0; i < n; ++i) {
