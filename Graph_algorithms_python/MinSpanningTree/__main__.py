5e7e2b3dacd8331a99640eba2393b01277702272
una1veritas <una.veritas@me.com>
2020-12-14 01:55:41 +0900

    python kruskal

--- Graph_algorithms_python/MinSpanningTree/__main__.py
+++ Graph_algorithms_python/MinSpanningTree/__main__.py
@@ -6,28 +6,33 @@ Created on 2020/01/14
 
 import sys
 
-def adjacents(G, point):
-    (V, E) = G
-    neighbors = set()
-    for (v, w) in E:
-        if v == point :
-            neighbors.add(w) # the other (2nd) end point
-        elif w == point :
-            neighbors.add(v)  # the 1st end point
-    return neighbors
+def cc_root(ccparent, v):
+    # find the root and update the parent = the root
+    parent = ccparent[v]
+    while ccparent[parent] != parent :
+        parent = ccparent[parent]
+    else:
+        ccparent[v] = parent
+    return parent
 
-#入力を集合として解釈
-
+def cc_connect(ccparent, u, v):
+    if cc_root(ccparent, u) < cc_root(ccparent, v) :
+        ccparent[ccparent[v]] = ccparent[u]
+    else:
+        ccparent[ccparent[u]] = ccparent[v]
+#入力を点集合と重み付き辺（辞書）として解釈
 if len(sys.argv) == 4 : 
     #引数から入力を得る場合
-    vertices = set(eval(sys.argv[1]))
-    edges = set(eval(sys.argv[2]))
-    weights = dict(eval(sys.argv[3]))
+    vstr = sys.argv[1]
+    westr = sys.argv[2]
 else:
     #プログラム内（ココ）で与える場合
-    vertices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l']
-    edges = [('a', 'b'), ('a', 'e'), ('a', 'i'), ('b', 'c'), ('b', 'f'), ('c', 'd'), ('c', 'f'), ('c', 'g'), ('d', 'h'), ('e', 'f'), ('e', 'i'), ('f', 'g'), ('f', 'j'), ('g', 'h'), ('g', 'k'), ('g', 'l'), ('h', 'l'), ('i', 'j'), ('j', 'k'), ('k', 'l')]
-    weight = {('a', 'e'): 17, ('a', 'b'): 5, ('a', 'i'): 19, ('b', 'c'): 11, ('b', 'f'): 10, ('c', 'f'): 6, ('c', 'd'): 15, ('c', 'g'): 9, ('d', 'h'): 4, ('e', 'f'): 16, ('e', 'i'): 28, ('f', 'g'): 1, ('f', 'j'): 18, ('g', 'h'): 13, ('g', 'k'): 26, ('g', 'l'): 27, ('h', 'l'): 12, ('i', 'j'): 8, ('j', 'k'): 2, ('k', 'l'): 24}
+    vstr = "['a', 'b', 'c', 'd', 'e', 'f', 'g']"
+    westr = "{('a', 'e'): 10, ('a', 'b'): 3, ('b', 'e'): 6, ('b', 'c'): 5, ('c', 'd'): 4, ('c', 'g'): 7, ('d', 'e'): 2, ('d', 'f'): 1, ('e', 'f'): 2, ('f', 'g'): 4}"
+
+vertices = eval(vstr)
+weight = eval(westr)
+edges = set(weight.keys())
 
 #グラフを定義（G に (V, E) を代入）
 G = (vertices, edges)
@@ -38,19 +43,17 @@ print('w =',weight)
 q = sorted(weight.items(), key=lambda elem:elem[1])
 
 #最小領域木を計算
-ccompos = dict()
+ccparent = dict()
 for i in vertices:
-    ccompos[i] = i
-tedges = set()
+    ccparent[i] = i  # the root is itself
+treeedges = set()
 for (anedge, aweight) in q:
     (u, v) = anedge
-    if ccompos[u] != ccompos[v] :
-        tedges.add(anedge)
-        if ccompos[u] > ccompos[v] :
-            (u, v) = (v, u)
-        print(u,v)
-        vrep = ccompos[v]
-        for i in vertices:
-            if ccompos[i] == vrep:
-                ccompos[i] = ccompos[u]
-        print(ccompos)
+    # check wether u and v are in the same connected components
+    if cc_root(ccparent, u) != cc_root(ccparent, v) :
+        treeedges.add(anedge)
+        #merge the components
+        cc_connect(ccparent, u, v)
+        print(ccparent)
+
+print("Et = ", treeedges)
