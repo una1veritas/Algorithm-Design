96e9aee5fb01a2c439654eb731180b6f8c9dd163
Sin Shimozono <una.veritas@icloud.com>
2021-05-13 16:16:05 +0900

    c

--- Levenshtein_distance/levdist.c
+++ Levenshtein_distance/levdist.c
@@ -4,11 +4,14 @@
 
 int levdist(const char * s, const char * t) {
 	int len_s = strlen(s), len_t = strlen(t);
-	int d[len_s+1][len_t+1];
+	int d[len_s+1][len_t+1]; 	// DP 表
+	// DP 表の最左列の初期化
 	for(int i = 0; i < len_s+1; ++i)
 		d[i][0] = i;
+	// DP 表の最上行の初期化
 	for(int i = 0; i < len_t+1; ++i)
 		d[0][i] = i;
+	// それ以外のセルの計算
 	int repl, inst, delt;
 	for(int i = 1; i < len_s+1; ++i) {
 		for(int j = 1; j < len_t+1; ++j) {
@@ -34,14 +37,26 @@ int levdist(const char * s, const char * t) {
 			}
 		}
 	}
-	//
+	// only for displaying the table
+	printf("   ");
+	for(int j = 0; j < len_t+1; ++j) {
+		printf("   %c ", (j == 0 ? ' ' : t[j-1]) );
+
+	}
+	printf("\n---");
+	for(int j = 0; j < len_t+1; ++j) {
+		printf("-----");
+	}
+	printf("\n");
 	for(int i = 0; i < len_s+1; ++i) {
+		printf("%c| ", (i == 0 ? ' ' : s[i-1]) );
 		for(int j = 0; j < len_t+1; ++j) {
 			printf("%4d ", d[i][j]);
 		}
 		printf("\n");
 	}
-
+	printf("\n");
+	//
 	return d[len_s][len_t];
 }
 
