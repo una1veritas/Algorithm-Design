b0c68133b91536960b9983faca07b506035c2b4a
Sin Shimozono <una.veritas@icloud.com>
2021-05-28 18:05:39 +0900

    test.c

--- C_Hashtable/test.c
+++ C_Hashtable/test.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char * argv[]) {
-  if (argc == 1) {
+  if (argc < 3) {
     return 1;
   }
-  int sum = 0;
-  for(char * p = argv[1]; *p; ++p) {
-    sum += *p;
+  long tablesize = atol(argv[1]);
+  for(char ** p = argv+2; *p; ++p) {
+    long k = atol(*p);
+    printf("%4ld to %4ld \n", k, k % tablesize);
   }
-  printf("sum = %d\n",sum);
+  printf("\n");
 
   return 0;
 }
