9916b537e2f816ee781825f4a3d3f7c70af79f63
Sin Shimozono <una.veritas@icloud.com>
2021-06-18 16:26:47 +0900

    main

--- PyMathConst/calcurator/__main__.py
+++ PyMathConst/calcurator/__main__.py
@@ -15,6 +15,8 @@ def pi(err = 0.01):
         i += 1
     return 2*(prev + piquarter)
 
+#ネイピア数 e をテイラー展開で求める．
+#剰余項の幅を定数の誤差範囲として評価する. 
 def napier(err = 0.01):
     esum = 1
     fac = 1
@@ -27,6 +29,19 @@ def napier(err = 0.01):
             break
     return esum
 
+#黄金比，フィボナッチ数列の前後二項の比を求める．
+#動的計画法的？にもとめる．
+def goldenratio(err = 0.0001):
+    f_0 = 1
+    f_1 = 1
+    f_2 = 2
+    r_pre = f_1 / f_0
+    while abs(f_1/f_0 - f_2/f_1) > err:
+        f_0 = f_1
+        f_1 = f_2
+        f_2 = f_0 + f_1
+    return f_2/f_1
+
 if len(sys.argv) > 1 and sys.argv[1] == 'pi' :
     err = 0.01
     if len(sys.argv) == 3 :
@@ -37,5 +52,10 @@ elif len(sys.argv) > 1 and sys.argv[1] == 'napier' :
     if len(sys.argv) == 3 :
         err = float(sys.argv[2])
     print(napier(err))
+elif len(sys.argv) > 1 and sys.argv[1] == 'goldenratio' :
+#    err = 0.01
+#    if len(sys.argv) == 3 :
+#        err = float(sys.argv[2])
+    print(goldenratio(0.0000000000000001))
     
 print('finished.')
