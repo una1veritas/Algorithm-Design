347af59e21131687082f37af4bb9eacf77ee5aa5
una1veritas <una.veritas@me.com>
2021-06-23 23:33:40 +0900

    math const

--- PyMathConst/calcurator/__main__.py
+++ PyMathConst/calcurator/__main__.py
@@ -1,69 +1,65 @@
 import sys
 from decimal import *
+from fractions import Fraction
 #import math
 
 #ライプニッツの式は π/4 を求めるものである．
 #ここでは i 番目の pi4 とその直前の値 prev の平均の 4 倍（和の2倍）としてπを求める
-def pi(prec = 6):
-    err = float('1E-'+str(prec))
-    piquarter = 1
-    sgn = -1
-    prev = 0
+def pi(prec):
+    prec = Decimal(prec/2)
+    pi4 = Decimal('1')
+    prev = Decimal('0')
     i = 1
-    chk = 1024
-    while abs(prev - piquarter) * 2 > err:
-        if i % chk == 0:
-            print(i, 4*piquarter, 4*prev)
-            chk *= 2
-        prev = piquarter
-        piquarter -= 1/float((i<<1)+1)
+    while abs(prev - pi4) > prec:
+        prev = pi4
+        pi4 -= Decimal('1')/((i<<1)+1)
         i += 1
-        prev = piquarter
-        piquarter += 1/float((i<<1)+1)
+        prev = pi4
+        pi4 += Decimal('1')/((i<<1)+1)
         i += 1
-    return 2*(prev + piquarter)
+    return 2*(prev + pi4)
 
 #ネイピア数 e をテイラー展開で求める．
 #剰余項の幅を定数の誤差範囲として評価する. 
-def napier(err = Decimal('0.001')):
-    esum = 1
-    fac = 1
+def napier(prec):
+    prec = Decimal(prec)
+    esum = Decimal('1')
+    fac = Decimal('1')
     i = 1
     while True :
         esum += 1/fac
         i += 1
         fac *= i
-        if (esum/(fac - 1) - 1/fac) <= err :
+        if (esum/(fac - 1) - 1/fac) <= prec :
             break
     return esum
 
 #黄金比，フィボナッチ数列の前後二項の比を求める．
 #動的計画法的？にもとめる．
-def goldenratio(err = 0.0001):
+def goldenratio(prec):
     f_0 = 1
     f_1 = 1
     f_2 = 2
-    r_pre = f_1 / f_0
-    while abs(f_1/f_0 - f_2/f_1) > err:
+    while abs(Fraction(f_1,f_0) - Fraction(f_2,f_1)) > prec:
         f_0 = f_1
         f_1 = f_2
         f_2 = f_0 + f_1
-    return f_2/f_1
+    return float(f_2)/f_1
 
 if len(sys.argv) > 1 and sys.argv[1] == 'pi' :
-    err = 0.01
+    prec = 0.01
     if len(sys.argv) == 3 :
-        prec = int(sys.argv[2])
+        prec = pow(10, -int(sys.argv[2]))
     print(pi(prec))
 elif len(sys.argv) > 1 and sys.argv[1] == 'napier' :
-    err = 0.01
+    prec = pow(10, -3)
     if len(sys.argv) == 3 :
-        err = float(sys.argv[2])
-    print(napier(err))
+        prec = pow(10, -int(sys.argv[2]))
+    print(napier(prec))
 elif len(sys.argv) > 1 and sys.argv[1] == 'goldenratio' :
-#    err = 0.01
-#    if len(sys.argv) == 3 :
-#        err = float(sys.argv[2])
-    print(goldenratio(0.0000000000000001))
+    prec = pow(10, -3)
+    if len(sys.argv) == 3 :
+        prec = pow(10, -int(sys.argv[2]))
+    print(goldenratio(prec))
     
 print('finished.')
