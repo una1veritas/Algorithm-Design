388964e5ee6c38905ffa89e104c063715015af04
Sin Shimozono <una.veritas@me.com>
2021-09-15 23:20:04 +0900

    line points

--- Line_Point_matching/src/Line_Point_matching.cpp
+++ Line_Point_matching/src/Line_Point_matching.cpp
@@ -10,38 +10,155 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <iomanip>
+
+#include <cmath>
 
 using namespace std;
 
-int read_csv(istream & ifile, std::vector<vector<double> > &array) {
-	std::istringstream strin;
-	std::string buff;
+struct Point {
+	double x, y;
 
-	if ( !ifile ) {
-		fprintf(stderr, "file open failed.\n");
-		return 0; // failed
+	Point(const double & _x, const double & _y) : x(_x), y(_y) {}
+
+	Point operator-(const Point & b) const{
+		return Point(x - b.x, y - b.y);
 	}
-	while (! ifile.eof()) {
-		double x;
-		getline(ifile, buff, '\n');
-		strin.str(buff);
-		strin.clear();
-		while ( !strin.eof() ) {
-			strin >> x;
-			std::cout << x << ", ";
-			std::cout.flush();
+
+	double distanceto(const Point & q) const {
+		double dx = x - q.x, dy = y - q.y;
+		return sqrt(dx*dx + dy*dy);
+	}
+
+	double norm_outer_prod(const Point & o, const Point & q) const {
+		Point op(*this - o), oq(q-o);
+		return (op.x * oq.y) - (op.y * oq.x);
+	}
+
+	double norm_inner_prod(const Point & o, const Point & q) const {
+		Point op(*this - o), oq(q-o);
+		return (op.x * oq.x) + (op.y * oq.y);
+	}
+
+	friend std::ostream & operator<<(std::ostream & out, const Point & me) {
+		out << me.x << ", " << me.y;
+		return out;
+	}
+};
+
+struct Line {
+	Point from, to;
+
+	Line(const Point & f, const Point & t) : from(f), to(t) {}
+	Line(const double & _x0, const double & _y0, const double & _x1, const double & _y1) :
+		from(_x0, _y0), to(_x1, _y1) {}
+
+	double length() const {
+		return from.distanceto(to);
+	}
+
+	double distanceto(const Point & p) const {
+		if ( to.norm_inner_prod(from, p) < 0 ) { // < 0.0
+			return to.distanceto(p);
 		}
-		std::cout << std::endl;
+		if ( from.norm_inner_prod(to, p) < 0 ) { // < 0.0
+			return from.distanceto(p);
+		}
+		double d = from.norm_outer_prod(to, p) / length();
+		if ( d < 0 )
+			return -d;
+		return d;
 	}
-	return 1; // succeeded
-}
+
+	 double nearest_matching_point_distance(const Point & p, const double & proximity) {
+		double len = length();
+		if (len == 0)
+			return 0;
+		double along = to.norm_inner_prod(from, p) / len;
+		double rev_along = from.norm_inner_prod(to, p) / len;
+		if (along < 0)
+			return 0;
+		if (along > len)
+			return len;
+		double dist = distanceto(p);
+		double base = sqrt(proximity*proximity - dist*dist);
+		std::cout << "proximity " << proximity << " along " << along << " dist " << dist << " base " << base << std::endl;
+		return along - base;
+	}
+
+	friend std::ostream & operator<<(std::ostream & out, const Line & me) {
+		out << me.from.x << ", " << me.from.y << " - "
+				<< me.to.x << ", " << me.to.y;
+		return out;
+	}
+};
+
+int read_csv(istream & ifile, std::vector<std::vector<std::string>> &tbl);
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 
-	ifstream ifile("test.csv");
-	vector<vector<double>> v;
-	read_csv(ifile, v);
-	ifile.close();
+	vector<vector<string>> tbl;
+	vector<Line> lines;
+	vector<Point> points;
+
+	ifstream linesfile("lines.csv");
+	read_csv(linesfile, tbl);
+	for(auto row = tbl.begin(); row != tbl.end(); ++row) {
+		double x0 = stod((*row)[0]), y0 = stod((*row)[1]), x1 = stod((*row)[2]), y1 = stod((*row)[3]);
+		lines.push_back(Line(x0, y0, x1, y1));
+	}
+	linesfile.close();
+	tbl.clear();
+	ifstream pointsfile("points.csv");
+	read_csv(pointsfile, tbl);
+	for(auto row = tbl.begin(); row != tbl.end(); ++row) {
+		double x0 = stod((*row)[0]), y0 = stod((*row)[1]);
+		points.push_back(Point(x0, y0));
+	}
+	pointsfile.close();
+
+	std::cout << "lines" << std::endl;
+	for(auto p = lines.begin(); p != lines.end(); ++p)
+		std::cout << *p <<std::endl;
+	std::cout << std::endl << "points" << std::endl;
+	for(auto p = points.begin(); p != points.end(); ++p)
+		std::cout << *p << std::endl;
+	std::cout <<std::endl;
+
+	Line & l = lines[0];
+	Point & p = points[0];
+	std::cout << l.length() << std::endl;
+	std::cout << l.distanceto(p) << std::endl;
+	std::cout << l.nearest_matching_point_distance(p, 0.5) << std::endl;
 	return 0;
+}
+
+int read_csv(istream & ifile, std::vector<std::vector<std::string>> &tbl) {
+	if ( !ifile ) {
+		fprintf(stderr, "file open failed.\n");
+		return 0; // failed
+	}
+	std::istringstream strin;
+	std::string buf;
+	size_t fromindex, toindex;
+	while (! ifile.eof()) {
+		getline(ifile, buf, '\n');
+		if ( buf.empty() )
+			continue;
+		tbl.push_back(std::vector<std::string>());
+		//std::cout << buf << std::endl;
+		unsigned int field;
+		for (fromindex = 0, field = 0; fromindex < buf.size(); fromindex = toindex + 1, ++field) {
+			toindex = buf.find_first_of(",", fromindex + 1);
+			if ( toindex == std::string::npos ) {
+				toindex = buf.size();
+			}
+			std::string sub(buf, fromindex, toindex - fromindex);
+			//std::cout << "[" << fromindex << ", " << toindex << "] " << sub << ", ";
+			tbl.back().push_back(sub);
+		}
+		//std::cout << std::endl;
+	}
+	return 1; // succeeded
 }
