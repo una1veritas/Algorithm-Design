b37e13ba8442e5f0acb9c1e2cf94e714594969f7
Sin Shimozono <una.veritas@icloud.com>
2022-06-09 17:52:26 +0900

    Update 2-3-4-Tree.cpp

--- 2-3-4-Tree/src/2-3-4-Tree.cpp
+++ 2-3-4-Tree/src/2-3-4-Tree.cpp
@@ -8,49 +8,53 @@
 
 #include <iostream>
 #include <vector>
-#include <cstring>
+#include <string>
 
 using namespace std;
 
-typedef const char * Data;
+typedef std::string Data;
 
 struct Node234 {
-	vector<Data> data;
-	vector<Node234 *> children;
+	Node234 * leftmost;
+	struct DataChild {
+		Data data;
+		Node234 * rightchild;
+
+		DataChild(const Data & d) : data(d), rightchild(NULL) {}
+	};
+	vector<DataChild> data234;
 
 	constexpr static unsigned int data_max_size = 3;
 	constexpr static unsigned int children_max_size = 4;
 
 	void initialize(void) {
-		data.clear();
-		children.clear();
+		leftmost = NULL;
+		data234.clear();
 	}
 
-	Node234(void) {
-		initialize();
-	}
+	Node234(void) : leftmost(NULL), data234() { }
 
-	void data_insert(const char * d) {
-		if ( data.size() == data_max_size ) {
-			cerr << "error: data_insert failure." << endl;
-			return;
+	bool insert_data(const std::string & str) {
+		if ( data234.size() >= data_max_size ) {
+			cerr << "error: insert_data failure." << endl;
+			return false;
 		}
-		vector<Data>::const_iterator i;
-		for (i = data.begin(); i != data.end() ; ++i) {
-			if ( strcmp(*i, d) > 0 )
+		auto i = data234.cbegin();
+		for ( ; i != data234.end() ; ++i) {
+			if ( i->data > str )
 				break;
 		}
-		if ( i == data.end() ) {
-			data.push_back(d);
+		if ( i == data234.end() ) {
+			data234.push_back(DataChild(str));
 		} else {
-			data.insert(i, d);
+			data234.insert(i, DataChild(str));
 		}
-		return;
+		return true;
 	}
 
 	friend ostream & operator<<(ostream & out, const Node234 & node) {
-		for(auto & d : node.data) {
-			out << d << ", ";
+		for(auto & dc : node.data234) {
+			out << dc.data << ", ";
 		}
 		out << endl;
 		return out;
@@ -64,10 +68,10 @@ int main(int argc, char * argv[]) {
 	}
 
 	Node234 tree;
-	tree.data_insert(argv[1]);
-	tree.data_insert(argv[2]);
-	tree.data_insert(argv[3]);
-	tree.data_insert(argv[4]);
+	tree.insert_data(argv[1]);
+	tree.insert_data(argv[2]);
+	tree.insert_data(argv[3]);
+	tree.insert_data(argv[4]);
 
 	cout << "Hello!" << endl;
 	cout << tree << endl;
