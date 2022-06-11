730f34713bc84babcd87fde416793c28841ae22e
Sin Shimozono <una.veritas@me.com>
2022-06-11 20:42:46 +0900

    2-3-4-Tree

--- 2-3-4-Tree/src/2-3-4-Tree.cpp
+++ 2-3-4-Tree/src/2-3-4-Tree.cpp
@@ -32,8 +32,8 @@ private:
 	constexpr static unsigned int children_max_size = 4;
 
 public:
-	Node234(const Data & d, Node234 * p, Node234 * l = NULL, Node234 * r = NULL) {
-		parent = p;
+	Node234(const Data & d, Node234 * par, Node234 * l = NULL, Node234 * r = NULL) {
+		parent = par;
 		data234.push_back(DataChild{d,l});
 		rightmostchild = r;
 	}
@@ -53,60 +53,85 @@ public:
 	}
 
 private:
-	const DataChild * find_data_or_gap(const Data & d) {
-		auto i = data234.cbegin();
-		for ( ; i != data234.cend() ; ++i) {
-			if ( !(i->data < d) )
+	unsigned int find_data_or_gap(const Data & d) {
+		unsigned int i;
+		for (i = 0 ; i < data234.size() ; ++i) {
+			if ( !(data234[i].data < d) )
 				break;
 		}
-		if (i != data234.cend())
-			return NULL;
-		return &(*i);
+		return i;
 	}
 
 	bool insert_data234(const Data & d) {
-		if ( data234.size() >= data_max_size ) {
+		if ( data234.size() == data_max_size ) {
 			cerr << "error: insert_data failure." << endl;
 			return false;
 		}
-		auto i = find_data_or_gap(d);
+		unsigned int i = find_data_or_gap(d);
 		DataChild dc = {d,NULL};
-		if ( i == NULL ) {
+		if ( i == data234.size() ) {
 			data234.push_back(dc);
 		} else {
-			data234.insert(i, dc);
+			data234.insert(data234.cbegin()+i, dc);
 		}
 		return true;
 	}
 
 public:
-	std::pair<Node234 *,DataChild *> find_node_and_dc(const Data & d) {
+	std::pair<Node234 *,unsigned int> find_node_and_dataix(const Data & d) {
 		Node234 * att = this;
 		for(;;) {
-			auto dcptr = att->find_data_or_gap(d);
-			if (dcptr == NULL) {
+			unsigned int i = att->find_data_or_gap(d);
+			if (i == att->data234.size()) {
 				att = rightmostchild;
 				if (att == NULL)
-					return std::pair<Node234 *,DataChild *>(this,NULL);
-			} else if (dcptr->data == d) {
-				return std::pair<Node234 *,DataChild *>(this, dcptr);
+					return std::pair<Node234 *,unsigned int>(this,i);
+			} else if (att->data234[i].data == d) {
+				return std::pair<Node234 *,unsigned int>(this, i);
 			} else {
-				att = dcptr->leftchild;
-				if (dcptr->leftchild == NULL)
-					return std::pair<Node234 *,DataChild *>(att,NULL);
+				att = att->data234[i].leftchild;
+				if (att == NULL)
+					return std::pair<Node234 *,unsigned int>(this,i);
 			}
 		}
-		return std::pair<Node234 *,DataChild *>(this,NULL);
+		return std::pair<Node234 *,unsigned int>(this,data234.size());
 	}
 
 	Node234 * insert(const Data & d) {
-		Node234 * current = this->find_node_and_dc(d);
-		bool stat;
-		auto itr = current->find_data_or_gap(d);
-		if (itr->data == d)
-			return current;
-		// d should be inserted.
+		auto nodeix = this->find_node_and_dataix(d);
+		Node234 * node = nodeix.first;
+		unsigned int ix = nodeix.second;
+		if (ix < node->data234.size())
+			if (node->data234[ix].data == d)
+				return node;
+		// d must be inserted.
+		if ( !node->is_full() ) {
+			node->insert_data234(d);
+			return node;
+		} else {
+			std::cerr << "full!" << std::endl;
+			// split
+			node->split();
+		}
 		return NULL;
+	}
+
+	void split() {
+		Node234 * rightsibling = new Node234(data234[2].data,parent,data234[2].leftchild,rightmostchild);
+		data234.pop_back();
+		Data & d = data234[1].data;
+		rightmostchild = data234[1].leftchild;
+		data234.pop_back();
+		if ( parent == NULL ) {
+			// make this node as parent
+			Node234 * leftsibling = this;
+			leftsibling->parent = this;
+			rightsibling->parent = this;
+			data234.pop_back();
+			data234.push_back(DataChild{d,leftsibling});
+			rightmostchild = rightsibling;
+		}
+
 	}
 
 	friend ostream & operator<<(ostream & out, const Node234 & node) {
