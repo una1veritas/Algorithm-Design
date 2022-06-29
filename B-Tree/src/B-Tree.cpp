7515b237376589be9c02fcec63f2ffbcd5eb8f14
Sin Shimozono <una.veritas@me.com>
2022-06-29 23:35:24 +0900

    btree

--- B-Tree/src/B-Tree.cpp
+++ B-Tree/src/B-Tree.cpp
@@ -7,15 +7,76 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+
+using std::cout;
+using std::endl;
+
+typedef std::string Key;
 
 struct BTreeNode {
-	static constexpr int MINIMUM_DEGREE = 3;
+private:
+	static constexpr int MINIMUM_DEGREE = 4;
 	static constexpr int MIN_KEYS = MINIMUM_DEGREE - 1;
 	static constexpr int MAX_KEYS = 2*MINIMUM_DEGREE - 1;
 
-	BtreeNode() {
+	BTreeNode * parent;
+	BTreeNode * rightmost;
+	struct {
+		Key key;
+		BTreeNode * left;
+	} keychild[MAX_KEYS];
+	unsigned int keycount;
 
+public:
+	BTreeNode(Key & k, BTreeNode * p = NULL) : parent(p), rightmost(NULL), keycount(0) {
+		keychild[keycount].key = k;
+		keychild[keycount].left = NULL;
+		++keycount;
+	}
+
+	bool is_root() const {
+		return parent == NULL;
+	}
+
+	BTreeNode * insert(Key & k) {
+		return NULL;
+	}
+
+	BTreeNode * find(Key & k) {
+		unsigned int ix;
+		BTreeNode * current = this;
+		BTreeNode * prev = this->parent;
+		for(;current != NULL;) {
+			for(ix = 0; ix < keycount; ++ix) {
+				if (keychild[ix].key >= k)
+					break;
+			}
+			if (keychild[ix].key == k) {
+				return current;
+			}
+			prev = current;
+			if (ix == keycount)
+				current = rightmost;
+			else
+				current = keychild[ix].left;
+		}
+		return current;
+	}
+};
+
+struct BTree {
+private:
+	BTreeNode * root;
+
+public:
+	BTree(void) : root(NULL) { }
+
+	BTreeNode * insert(Key & k) {
+		if (root == NULL) {
+			return root = new BTreeNode(k);
+		}
+		root->find_parent(k);
+		return root->insert(k);
 	}
 };
 
