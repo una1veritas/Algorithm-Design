7ba81f194763fb22f75a6e204e0a3a4fb4b8848e
Sin Shimozono <una.veritas@me.com>
2022-07-27 02:57:11 +0900

    union-find

--- Union-Find/src/Union-Find.cpp
+++ Union-Find/src/Union-Find.cpp
@@ -13,27 +13,48 @@ using std::endl;
 class UnionFindSet {
 protected:
 	unsigned int * parent;
+	unsigned char * rank;
 	unsigned int number;
 
 public:
 	UnionFindSet(const unsigned int & n) : number(n) {
 		parent = new unsigned int [number];
-		for(unsigned int i = 0; i < number; ++i)
-			parent[i] = i; // follow itself.
+		rank = new unsigned char [number];
+		for(unsigned int i = 0; i < number; ++i) {
+			parent[i] = i;
+			rank[i] = 0;
+		}
 	}
 
 	virtual ~UnionFindSet() {
 		//cout << "~UnionFindSet " << endl;
 		delete [] parent;
+		delete [] rank;
 	}
 
 	// path halving
+	/*
 	virtual unsigned int find_set(unsigned int x) {
 		unsigned int child;
 		while (x != parent[x]) {
 			child = x;
 			x = parent[x];
 			parent[child] = parent[x];
+		}
+		return x;
+	}
+	*/
+
+	// path compression
+	unsigned int find_set(unsigned int x) {
+		unsigned int root = x;
+		while (root != parent[root])
+			root = parent[root];
+		unsigned int t;
+		while (x != parent[x]) {
+			t = x;
+			x = parent[x];
+			parent[t] = root;
 		}
 		return x;
 	}
@@ -44,30 +65,35 @@ public:
 		return x;
 	}
 
-	virtual unsigned int merge(unsigned int x, unsigned int y) {
+	unsigned int union_set(unsigned int x, unsigned int y) {
 		x = find_set(x);
 		y = find_set(y);
-		if ( x == y ) {
-			return x;
-		} else if (x < y) {
+		if (rank[x] == rank[y]) {
+			if (x < y) {
+				parent[y] = x;
+				rank[x] += 1;
+			} else {
+				parent[x] = y;
+				rank[y] += 1;
+			}
+		} else if ( rank[x] > rank[y] ) {
 			parent[y] = x;
-			return x;
-		} else {
+		} else /* if (rank[x] <= rank[y]) */ {
 			parent[x] = y;
-			return y;
 		}
+		return parent[x];
 	}
 
-	bool in_the_same_group(const unsigned int & x, const unsigned int & y) {
+	bool in_the_same(const unsigned int & x, const unsigned int & y) {
 		return find_set(x) == find_set(y);
 	}
 
-	bool in_the_same_group(const unsigned int & x, const unsigned int & y) const {
+	bool in_the_same(const unsigned int & x, const unsigned int & y) const {
 		return find_set(x) == find_set(y);
 	}
 
-	virtual std::ostream & node_str(std::ostream & out, const unsigned int & x) const {
-		out << x << "[" << parent[x] << "] ";
+	std::ostream & node_str(std::ostream & out, const unsigned int & x) const {
+		out << x << "[" << parent[x] << ":" << int(rank[x]) << "] ";
 		return out;
 	}
 
@@ -87,7 +113,7 @@ public:
 				comma = true;
 			cout << "{";
 			for(unsigned int i = 0; i < ufs.number; ++i) {
-				if ( ufs.in_the_same_group(x,i) ) {
+				if ( ufs.in_the_same(x,i) ) {
 					if ( x != i )
 					cout << ", ";
 					ufs.node_str(cout, i);
@@ -101,83 +127,27 @@ public:
 	}
 };
 
-class RankedUnionFindSet : public UnionFindSet {
-	unsigned int * rank;
-
-public:
-	RankedUnionFindSet(const unsigned int & n) :
-		UnionFindSet(n) {
-		rank = new unsigned int [number];
-		for(unsigned int i = 0; i < number; ++i)
-			rank[i] = 0;
-	}
-
-	~RankedUnionFindSet() {
-		//cout << "~RankedUnionFindSet " << endl;
-		delete [] rank;
-	}
-
-	unsigned int find_set(unsigned int x) {
-		unsigned int root = x;
-		while (root != parent[root])
-			root = parent[root];
-		unsigned int t;
-		while (x != parent[x]) {
-			t = x;
-			x = parent[x];
-			parent[t] = root;
-		}
-		return x;
-	}
-	unsigned int merge(unsigned int x, unsigned int y) {
-		x = find_set(x);
-		y = find_set(y);
-		if (rank[x] == rank[y]) {
-			if (x < y) {
-				parent[y] = x;
-				rank[x] += 1;
-			} else {
-				parent[x] = y;
-				rank[y] += 1;
-			}
-		} else if ( rank[x] > rank[y] ) {
-			parent[y] = x;
-		} else /* if (rank[x] <= rank[y]) */ {
-			parent[x] = y;
-		}
-		return parent[x];
-	}
-
-	std::ostream & node_str(std::ostream & out, const unsigned int & x) const {
-		out << x << "[" << parent[x] << ":" << rank[x] << "] ";
-		return out;
-	}
-};
-
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	RankedUnionFindSet ufs(8);
+	UnionFindSet ufs(8);
 	std::cout << ufs << endl;
-	ufs.merge(6,7);
+	ufs.union_set(6,7);
 	std::cout << ufs << endl;
-	ufs.merge(4,5);
+	ufs.union_set(4,5);
 	std::cout << ufs << endl;
-	ufs.merge(3,4);
+	ufs.union_set(3,4);
 	std::cout << ufs << endl;
-	ufs.merge(2,3);
+	ufs.union_set(2,3);
 	std::cout << ufs << endl;
-	ufs.merge(2,6);
+	ufs.union_set(2,6);
 	std::cout << ufs << endl;
-	ufs.merge(1,2);
+	ufs.union_set(1,2);
 	std::cout << ufs << endl;
-	ufs.merge(1,0);
+	ufs.union_set(1,0);
 	std::cout << ufs << endl << endl;
 	//std::cout << "find_set " <<  ufs.find_set(5) << endl;
 	//std::cout << ufs << endl << endl;
 
-
-	RankedUnionFindSet s(4);
-	std::cout << s << endl;
 	return 0;
 }
