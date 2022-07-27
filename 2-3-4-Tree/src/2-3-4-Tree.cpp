086ee18caa731d47d4617040588d59b50be2d2ad
Sin Shimozono <una.veritas@icloud.com>
2022-07-27 16:19:44 +0900

    2-3-4

--- 2-3-4-Tree/src/2-3-4-Tree.cpp
+++ 2-3-4-Tree/src/2-3-4-Tree.cpp
@@ -25,6 +25,13 @@ private:
 	constexpr static unsigned int children_max_size = 4;
 
 public:
+	// construct a stab node.
+	Node234(void) {
+		parent = NULL;
+		childptr[0] = NULL;
+		keycount = 0;
+	}
+
 	Node234(const Key & k, Node234 * par = NULL, Node234 * left = NULL, Node234 * right = NULL)
 	: parent(par), keycount(0) {
 		// construct a 2-node.
@@ -42,8 +49,12 @@ public:
 		keycount = 0;
 	}
 
+	bool is_stub() const {
+		return keycount == 0;
+	}
+
 	bool is_root() const {
-		return parent == NULL;
+		return parent == NULL or parent->is_stub();
 	}
 
 	bool is_leaf() const {
@@ -57,7 +68,7 @@ public:
 	}
 
 private:
-	unsigned int key_lowerbound(const Key & k) {
+	unsigned int key_upperbound_index(const Key & k) {
 		// assuming one or more key(s) exist(s).
 		unsigned int i;
 		for (i = 0 ; i < keycount ; ++i) {
@@ -67,134 +78,116 @@ private:
 		return i;
 	}
 
-	unsigned int insert_key_to_node(const Key & k) {
-		if ( data234.size() == data_max_size ) {
+	unsigned int insert_key_to_node(const Key & k, Node234 * key_rightchild = NULL) {
+		if ( is_full() ) {
 			cerr << "error: insert_in_data234 failure." << endl;
-			return data_max_size;
+			return key_max_size;
 		}
-		unsigned int i = data_lowerbound(d);
-		DataChild dc = {d,NULL};
-		if ( i == data234.size() ) {
-			data234.push_back(dc);
-		} else {
-			data234.insert(data234.cbegin()+i, dc);
+		if (keycount == 0) {
+			keyptr[0] = &k;
+			childptr[0] = NULL;
+			childptr[1] = NULL;
+			keycount = 1;
+			return 0;
 		}
+		unsigned int i = key_upperbound_index(k);
+		for(unsigned int j = keycount; j > i; --j) {
+			keyptr[j] = keyptr[j-1];
+			childptr[j+1] = childptr[j];
+		}
+		keyptr[i] = &k;
+		childptr[i+1] = key_rightchild;
+		keycount += 1;
 		return i;
 	}
 
-	std::pair<Node234 *,unsigned int> find_insert_position(const Data & d, const bool findBottom = true) {
+	Node234 * find_insert_node(const Key & k, const bool findBottom = true) {
 		Node234 * att = this;
-		unsigned int i;
 		for(;;) {
 			//std::cout << "find " << d << " in " << *att << std::endl;
 			if (!att->is_leaf() and att->is_full()) {
-				std:cout << "encountered a node must be splitted." << std::endl;
-				att = att->split();
-
+				std::cout << "encountered a node must be splitted." << std::endl;
+				att->split();
+				att = att->parent;
 			}
-			i = att->data_lowerbound(d);
+			if ( att->is_leaf() )
+				break;
+			unsigned int i = att->key_upperbound_index(k);
 			//std::cout << " pos= " << i << std::endl;
-			if (i == att->data234.size()) {
-				//std::cout << "here!" << std::endl;
-				if (att->rightmostchild != NULL) {
-					//std::cout << "gogo!" << std::endl;
-					att = att->rightmostchild;
+			att = childptr[i];
+			if ( i < keycount and *keyptr[i] == k) {
+				if ( findBottom )
 					continue;
-				} else {
-					break;
-				}
-			} else if (att->data234[i].data == d) {
-				if (! findBottom )
-					break;
-				if (att->data234[i].leftchild == NULL)
-					break;
-				att = att->data234[i].leftchild;
-
-			} else {
-				if ( att->data234[i].leftchild != NULL ) {
-					att = att->data234[i].leftchild;
-					continue;
-				} else {
-					break;
-				}
+				break;
 			}
 		}
-		std::cout << std::endl;
-		return std::pair<Node234 *,unsigned int>(att,i);
+		return att;
 	}
 
 public:
-	Node234 * insert(const Data & d) {
+	Node234 * insert(const Key & k) {
 		// std::cout << "inserting " << d << std::endl;
-		auto nodeix = this->find_insert_position(d);
-		Node234 * node = nodeix.first;
-		unsigned int ix = nodeix.second;
-		/*
-		if (ix < node->data234.size()) {
-			if (node->data234[ix].data == d) {
-				// d already exists in the tree.
-				return node;
-			}
-		}
-		*/
+		Node234 * node = this->find_insert_node(k, true);
+
 		if ( !node->is_full() ) {
-			node->insert_in_node(d);
+			node->insert_key_to_node(k);
 			return node;
 		} else {
 			// split
-			//std::cout << *node << " will be splitted." << std::endl;
-			node = node->split();
-			//std::cout << "after the split " << *node << std::endl;
-			auto nextnodeix = node->find_insert_position(d);
-			//std::cout << "adding to " << *(nextnodeix.first) << std::endl;
-			nextnodeix.first->insert_in_node(d);
+			std::cout << "encountered full-node to insert " << *node << std::endl;
 		}
 		return NULL;
 	}
 
-	Node234 * split() {
-		if (data234.size() != data_max_size) {
-			std::cout << "error! try to split the not-full node." << std::endl;
-			return NULL;
+	void split() {
+		if ( !is_full() ) {
+			std::cout << "error! tried to split a not-full node." << std::endl;
+			return;
 		}
 		Node234 * left, *right;
 		if ( is_root() ) {
-			//std::cout << "going to split the root." << std::endl;
-			right = new Node234(data234[2].data,this,data234[2].leftchild,rightmostchild);
-			left= new Node234(data234[0].data,this,data234[0].leftchild,data234[1].leftchild);
-			data234[0] = data234[1];
-			data234.pop_back(); data234.pop_back();
-			data234[0].leftchild = left;
-			rightmostchild = right;
-			return this;
+			std::cout << "going to split the root." << std::endl;
+			right = new Node234(*keyptr[2], this, childptr[2],childptr[3]);
+			left= new Node234(*keyptr[0], this, childptr[0], childptr[1]);
+			keyptr[0] = keyptr[1];
+			childptr[0] = childptr[1];
+			childptr[1] = childptr[2];
+			keycount = 1;
+			return;
 		}
-		right = new Node234(data234[2].data,parent,data234[2].leftchild,rightmostchild);
-		data234.pop_back();
-		unsigned int pos = parent->insert_in_node(data234[1].data);
-		parent->data234[pos].leftchild = this;
-		if (pos + 1 < parent->data234.size()) {
-			parent->data234[pos+1].leftchild = right;
-		} else {
-			parent->rightmostchild = right;
-		}
-		rightmostchild = data234[1].leftchild;
-		data234.pop_back();
-		return parent;
+		right = new Node234(*keyptr[2], parent, childptr[2], childptr[3]);
+		unsigned int pos = parent->insert_key_to_node(*keyptr[1]);
+		parent->childptr[pos+1] = this;
+		keycount = 1;
+		return;
 	}
 
 	friend ostream & operator<<(ostream & out, const Node234 & node) {
 		out << "(";
-		for(unsigned int i = 0; i < node.data234.size(); ++i) {
-			if (node.data234[i].leftchild != NULL) {
-				out << * node.data234[i].leftchild;
+		for(unsigned int i = 0; i < node.keycount; ++i) {
+			if (node.childptr[i] != NULL) {
+				out << * node.childptr[i];
 				out << ", ";
 			}
-			out << node.data234[i].data << ",";
+			out << * node.keyptr[i] << ",";
 		}
-		if (node.rightmostchild != NULL)
-			out << *node.rightmostchild;
+		if (node.childptr[node.keycount] != NULL)
+			out << * node.childptr[node.keycount];
 		out << ")";
 		return out;
+	}
+
+	friend Node234;
+};
+
+struct Tree234 {
+	Node234 root;
+public:
+
+	Tree234() : root() {	}
+
+	void insert(const Key & k) {
+		root.insert(k);
 	}
 };
 
