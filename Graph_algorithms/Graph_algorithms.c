58621287ad8afc581ebb86f68e0e0be09d18c0aa
una1veritas <una.vertas@icloud.com>
2022-12-17 03:07:51 +0900

    graph

--- Graph_algorithms/Graph_algorithms.c
+++ Graph_algorithms/Graph_algorithms.c
@@ -7,6 +7,7 @@ typedef struct Edge {
 	int u, v;
 } Edge;
 
+
 typedef struct Graph {
 	int vertices[MAX_VERTICES];
 	int vsize;
@@ -14,7 +15,10 @@ typedef struct Graph {
 	int esize;
 } Graph;
 
-void Graph_int(Grapg * gp, Vertex v[], int n, Edge e[], int m) {
+void Graph_add_vertex(Graph * gp, int v);
+void Graph_add_edge(Graph * gp, Edge e);
+
+void Graph_init(Graph * gp, Vertex v[], int n, Edge e[], int m) {
 	gp->vsize = 0;
 	gp->esize = 0;
 	for(int i = 0; i < n; ++i) {
@@ -26,59 +30,65 @@ void Graph_int(Grapg * gp, Vertex v[], int n, Edge e[], int m) {
 	return;
 }
 
-void graph_add_vertex(graph * g, int v) {
+void Graph_add_vertex(Graph * gp, int v) {
 	int i;
-	for(i = 0; i < g->vsize; i += 1) {
-		if (g->vertices[i] == v)
+	// インサーションソート（昇順）で追加
+	for(i = 0; i < gp->vsize; i += 1) {
+		if (gp->vertices[i] == v)
 			return;
-		if (g->vertices[i] > v)
+		if (gp->vertices[i] > v)
 			break;
 	}
-	g->vsize += 1;
-	for(int j = g->vsize - 1; j >= i; j -= 1) {
-		g->vertices[j] = g->vertices[j-1];
+	gp->vsize += 1;
+	for(int j = gp->vsize - 1; j >= i; j -= 1) {
+		gp->vertices[j] = gp->vertices[j-1];
 	}
-	g->vertices[i] = v;
+	gp->vertices[i] = v;
 }
 
-void graph_add_edge(graph * g, edge e) {
+void Graph_add_edge(Graph * gp, Edge e) {
+	int t;
+	if ( e.u > e.v ) {
+		t = e.u; e.u = e.v; e.v = t;
+	}
+	// インサーションソート（辞書式昇順）で追加
 	int i;
-	if ( e[0] > e[1] ) {
-		i = e[1]; e[1] = e[0]; e[0] = i;
-	}
-	for(i = 0; i < g->esize; i += 1) {
-		if ( g->edges[i][0] == e[0] && g->edges[i][1] == e[1] )
+	for(i = 0; i < gp->esize; ++i) {
+		if ( gp->edges[i].u == e.u && gp->edges[i].v == e.v ) {
+			// すでにある
 			return;
-		if ( g->edges[i][0] > e[0] || (g->edges[i][0] == e[0] && g->edges[i][1] > e[1]) )
+		}
+		if ( gp->edges[i].u > e.u || (gp->edges[i].v > e.v) ) {
+			// 追加する辺よりも後になる辺が見つかった
 			break;
+		}
 	}
-	g->esize += 1;
-	for(int j = g->esize - 1; j >= i; j -= 1) {
-		g->edges[j][0] = g->edges[j-1][0];
-		g->edges[j][1] = g->edges[j-1][1];
+	gp->esize += 1;
+	for(int j = gp->esize - 1; j >= i; j -= 1) {
+		gp->edges[j] = gp->edges[j-1];
 	}
-	g->edges[i][0] = e[0];
-	g->edges[i][1] = e[1];
+	gp->edges[i] = e;
 }
 
 
 
-void graph_print(graph * g) {
-	printf("({");
-	for(int i = 0; i < g->vsize; ++i) {
-		printf("%d, ", g->vertices[i]);
+void Graph_print(Graph * gp) {
+	printf("G=({");
+	for(int i = 0; i < gp->vsize; ++i) {
+		printf("%d, ", gp->vertices[i]);
 	}
 	printf("}, {");
-	for(int i = 0; i < g->esize; ++i) {
-		printf("(%d, %d), ", g->edges[i][0], g->edges[i][1]);
+	for(int i = 0; i < gp->esize; ++i) {
+		printf("(%d, %d), ", gp->edges[i].u, gp->edges[i].v);
 	}
 	printf(")\n");
 }
 
 int main(int argc, char **argv) {
-	int v[] = {1, 6, 2, 3, 4, 5, };
-	edge e[] ={{1, 2}, {2, 3}, {3, 4}, {5, 6}, {6, 1}};
-	graph g = graph_init(v, 6, e, 5);
-	graph_print(&g);
+	int vertices[] = {1, 6, 2, 3, 4, 5, };
+	Edge edges[] ={{1, 2}, {2, 3}, {3, 4}, {5, 6}, {6, 1}};
+	Graph g;
+	Graph_init(&g, vertices, 6, edges, 5);
+	Graph_print(&g);
 	return 0;
 }
