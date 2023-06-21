ad3e8ca1e4abd66c961358ffce87c5bbc4a54c7c
Sin Shimozono <una.veritas@icloud.com>
2023-06-21 18:22:38 +0900

    main.c

--- C_sortalgorithms/src/main.c
+++ C_sortalgorithms/src/main.c
@@ -39,6 +39,7 @@ int main(int argc, char **argv) {
 	//bucket_sort(a, num, 0, 201, keyval);
 	//bucket_sort_fixed(a, num, 0, 101, keyval);
 
+	printf("result:\n");
 	for(long i = 0; i < num; ++i) {
 		printf("%s, ", *a[i]);
 	}
@@ -50,3 +51,25 @@ int main(int argc, char **argv) {
 }
 
 // 55 11 26 17 86 102 43 11 98
+/*
+ * hektor, summicron, elmarit, nokton, planar, sonnar, distagon, xenotar, tessar,
+making heap..
+heap:
+xenotar tessar sonnar summicron planar elmarit distagon nokton hektor
+
+result:
+distagon, elmarit, hektor, nokton, planar, sonnar, summicron, tessar, xenotar,
+ *
+ */
+
+/*
+ * summilux,, elmar,, noctilux,, planar,, sonnar,, distagon,, xenotar,, tessar,
+making heap..
+heap:
+xenotar, tessar summilux, planar, sonnar, distagon, noctilux, elmar,
+
+result:
+distagon,, elmar,, noctilux,, planar,, sonnar,, summilux,, tessar, xenotar,,
+ *
+ */
+
