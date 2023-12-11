60eac1cde0df385444cddb64bf07c515362892c9
Sin Shimozono <una.veritas@icloud.com>
2023-12-11 16:09:21 +0900

    sample

--- Graph_sample/src/graph_sample.c
+++ Graph_sample/src/graph_sample.c
@@ -22,14 +22,35 @@ int adjacent_vertices(edge * edges, int u, int v) {
 }
 
 int main(int agrc, char * argv[]) {
-	int size = 5;
+	const int size = 5; // 辺の最大数は size*(size-1)/2; 定数 const として宣言
 	char * labels[] = { "A", "B", "C", "D", "E" };      // 名前は size 個必要．
-	edge edges[] = { {0,1}, {0,2}, {1,3}, {2,3}, {0, 0} };    // 長さ 2 の配列が一つの辺．
+
+	// 辺を整数の組（長さ2の配列）で，辺の集合をその配列で表す方法．
+	edge edges[] = { {0,1}, {0,3}, {1,2}, {1,3}, {1,4}, {2,3}, {2,4}, {0,0} };    // 長さ 2 の配列が一つの辺．
+
+	// 辺を隣接行列（size x size の整数の二次元配列）で表す方法
+	int adjmatrix[size][size] = {
+			{0,1,0,1,0},
+			{1,0,1,1,1},
+			{0,1,0,1,1},
+			{1,0,1,0,0},
+			{0,1,1,0,0},
+	};
+
+	// 辺を隣接リストで表す方法
+	// 便宜的に size x size の配列に収めているが，本来は可変長
+	int adjlist[size][size] = {
+			{1, 3, 0},
+			{2, 3, 4, 0},
+			{3, 4, 0},
+			{0},
+			{0},
+	};
 
 	printf("%d\n", size);
 	for(int i = 0; i < size; ++i)
 		printf("%s\n", labels[i]);
-	for(int i = 0; edges[i].u != 0 || edges[i].v != 0; ++i)
+	for(int i = 0; !(edges[i].u == 0 && edges[i].v == 0); ++i)
 		printf("(%d, %d)\n", edges[i].u, edges[i].v);
 	if (adjacent_vertices(edges, 3, 1)) {
 		printf("yes\n");
