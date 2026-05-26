f805fcbda34a89b508693300810251cec3e9001c
Sin Shimozono <una.veritas@icloud.com>
2026-05-26 12:47:52 +0900

    Test

---
+++ HelloJava/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Why?");
+		
+		int i = 0;
+		for( ; i < 5; ++i) {
+			System.out.println("Test");
+		}
+
+	}
+
+}
