d0dfbaaab3f482247d794b2e7c76e2692ff1e18f
Sin Shimozono <una.veritas@me.com>
2026-06-03 10:39:39 +0900

    shell sort

--- C_sortalgorithms/src/shell_sort.c
+++ C_sortalgorithms/src/shell_sort.c
@@ -13,7 +13,7 @@ void shell_sort(data * a[], long n, int (* less)(const void *, const void *)) {
     for (long gap = n>>1; gap > 0; gap >>= 1) {
         fprintf(stdout, "gap = %ld\n", gap);
 
-        // Perform a "gapped" insertion sort for this gap size
+        // Perform a "gapped" swap, from shortest length-1 chain to longer ones
         for (long i = gap; i < n; i++) {
             fprintf(stdout, "i = %ld\n", i);
             for(int i = 0; i < n; ++i) {
@@ -22,14 +22,15 @@ void shell_sort(data * a[], long n, int (* less)(const void *, const void *)) {
             fprintf(stdout, "\n");
 
             // Current element to be placed correctly
-            data * tmp;
+            data * tmp = a[i];
 
+            // j - gap < j
             // Shift elements that are greater than temp to make space
-            for (long j = i; j >= gap && less(a[j], a[j - gap]); j -= gap ) {
-            	tmp = a[j];
+            long j;
+            for (j = i; j >= gap && less(tmp, a[j - gap]); j -= gap ) {
                 a[j] = a[j - gap];
-                a[j - gap] = tmp;
             }
+            a[j] = tmp;
 
             for(int i = 0; i < n; ++i) {
             	fprintf_data(stdout, a[i]);
