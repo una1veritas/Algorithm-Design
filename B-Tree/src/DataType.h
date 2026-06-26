f5f3e063daec3236e0e10f1ae2465fac743493bb
Sin Shimozono <una.veritas@me.com>
2026-06-27 01:25:48 +0900

    data type

---
+++ B-Tree/src/DataType.h
@@ -0,0 +1,18 @@
+/*
+ * DataType.h
+ *
+ *  Created on: 2026/06/17
+ *      Author: sin
+ */
+
+#ifndef DATATYPE_H_
+#define DATATYPE_H_
+
+#include <string>
+
+typedef std::string DataType;
+
+// required operators or functions are supported by class std::string.
+
+
+#endif /* DATATYPE_H_ */
